afb89d068b1cb5743ef2041ba2be5a9985e888ed
Santy-escc <santy.escc@gmail.com>
2022-10-05 18:51:04 -0500

    step2

---
+++ interacciones.py
@@ -0,0 +1,19 @@
+"""cont = 0
+while cont < 10 :
+    print (cont)
+    cont +=1 
+
+iters = ({'a', 'b', 'c', 'd'})
+
+for x in iters :
+    print(x) """
+
+x = 0.0
+for i in range(10):
+    x += 0.1
+    print (x)
+
+if x == 1.0:
+    print(f'x = {x}')
+else:
+    print(f'x != {x}')
